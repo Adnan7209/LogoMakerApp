6bad90d907fa1ccf8e3d418db99c3267bba0e0f4
Adnan7209 <mdadnan28713@gmail.com>
2024-05-11 17:02:39 +0530

    background round,padding & color added ...code formated with prettier

--- src/components/custom/BackgroundController.jsx
+++ src/components/custom/BackgroundController.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Slider } from "../ui/slider";
+import ColorPickerController from "./ColorPickerController";
 
 const BackgroundController = () => {
-  return (
-    <div>BackgroundController</div>
-  )
-}
+    const storedValues= JSON.parse(localStorage.getItem('value'));
+    const getInitialValue = (key, defaultValue) => {
+        return storedValues ? storedValues[key] : defaultValue;
+      };
+    
+      const [rounded, setRounded] = useState(() => getInitialValue('bgRounded', 0));
+      const [padding, setPadding] = useState(() => getInitialValue('bgPadding', 0));
+      const [bgColor, setBgColor] = useState(() => getInitialValue('bgColor', 'rgba(255,255,255,1)'));
+  
 
-export default BackgroundController
+    useEffect(()=>{
+        const  updatedValue = {
+            ...storedValues,
+            bgRounded:rounded,
+            bgPadding:padding,
+            bgColor:bgColor,
+        }
+        localStorage.setItem('value',JSON.stringify(updatedValue));
+
+    },[rounded,padding,bgColor])
+  return (
+    <div>
+      <div className="p-2">
+        <label className="p-2 flex justify-between items-center">
+          Rounded <span>{rounded}px</span>
+        </label>
+        <Slider
+          defaultValue={rounded}
+          max={100}
+          step={1}
+          onValueChange={(event) => setRounded(event)}
+        />
+      </div>
+      <div className="p-2">
+        <label className="p-2 flex justify-between items-center">
+          Padding <span>{padding}px</span>
+        </label>
+        <Slider
+          defaultValue={padding}
+          max={100}
+          step={1}
+          onValueChange={(event) => setPadding(event)}
+        />
+      </div>
+      <div className='p-2'>
+                <label className='p-2 flex justify-between items-center'>Color</label>
+                <ColorPickerController color={bgColor} setColor={setBgColor}/>
+                
+            </div>
+    </div>
+  );
+};
+
+export default BackgroundController;
--- src/components/custom/IconController.jsx
+++ src/components/custom/IconController.jsx
@@ -1,53 +1,69 @@
-import { Smile } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
-import { Slider } from '../ui/slider'
-import ColorPickerController from './ColorPickerController';
+import { Smile } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { Slider } from "../ui/slider";
+import ColorPickerController from "./ColorPickerController";
 
 const IconController = () => {
-    const [size,setSize] = useState(150);
-    const [rotate,setRotate] = useState(0);
-    const [color, setColor] = useState('rgba(255,255,255,1)');
+  const storedValues = JSON.parse(localStorage.getItem("value"));
 
-    const storageValue = JSON.parse(localStorage.getItem('value'));
+  const getInitialValue = (key, defaultValue) => {
+    return storedValues ? storedValues[key] : defaultValue;
+  };
 
-    useEffect(()=>{
-        const  updatedValue = {
-            ...storageValue,
-            iconsSize:size,
-            iconRotate:rotate,
-            iconColor:color,
-            icon:'Smile'
-        }
-        localStorage.setItem('value',JSON.stringify(updatedValue));
+  const [size, setSize] = useState(() => getInitialValue("iconSize", 0));
+  const [rotate, setRotate] = useState(() => getInitialValue("iconRotate", 0));
+  const [color, setColor] = useState(() =>
+    getInitialValue("iconColor", "rgba(255,255,255,1)")
+  );
 
-    },[size,rotate,color])
+
+  useEffect(() => {
+    const updatedValue = {
+      ...storedValues,
+      iconSize: size,
+      iconRotate: rotate,
+      iconColor: color,
+      icon: "Smile",
+    };
+    localStorage.setItem("value", JSON.stringify(updatedValue));
+  }, [size, rotate, color]);
   return (
     <div>
-        <div>
-            <label>Icon</label>
-            <div className='p-3 cursor-pointer bg-gray-200 rounded-md w-[150px] h-[150px] flex items-center justify-center my-2'>
-            <Smile />
-            </div>
-            <div className='p-2'>
-                <label className='p-2 flex justify-between items-center'>Size <span>{size}px</span></label>
-                <Slider defaultValue={[150]} max={512} step={1} 
-                  onValueChange={(event)=>setSize(event)}
-                />
-            </div>
-            <div className='p-2'>
-                <label className='p-2 flex justify-between items-center'>Rotate <span>{rotate}°</span></label>
-                <Slider defaultValue={[0]} max={360} step={1} 
-                  onValueChange={(event)=>setRotate(event)}
-                />
-            </div>
-            <div className='p-2'>
-                <label className='p-2 flex justify-between items-center'>Color</label>
-                <ColorPickerController color={color} setColor={setColor}/>
-                
-            </div>
+      <div>
+        <label>Icon</label>
+        <div className="p-3 cursor-pointer bg-gray-200 rounded-md w-[150px] h-[150px] flex items-center justify-center my-2">
+          <Smile />
         </div>
+        <div className="p-2">
+          <label className="p-2 flex justify-between items-center">
+            Size <span>{size}px</span>
+          </label>
+          <Slider
+            defaultValue={size}
+            max={512}
+            step={1}
+            onValueChange={(event) => setSize(event)}
+          />
+        </div>
+        <div className="p-2">
+          <label className="p-2 flex justify-between items-center">
+            Rotate <span>{rotate}°</span>
+          </label>
+          <Slider
+            defaultValue={rotate}
+            min={-360}
+            max={360}
+            step={1}
+            onValueChange={(event) => setRotate(event)}
+          />
+        </div>
+        <div className="p-2">
+          <label className="p-2 flex justify-between items-center">Color</label>
+          <ColorPickerController color={color} setColor={setColor} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default IconController
+export default IconController;
