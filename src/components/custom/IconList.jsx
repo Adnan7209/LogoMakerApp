59757461eb4dfa3f57769d437340eb1c16fdd4c9
Adnan7209 <mdadnan28713@gmail.com>
2024-05-14 03:38:13 +0530

    search functionality added in change icon list

--- src/components/custom/IconList.jsx
+++ src/components/custom/IconList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { iconNames } from "@/utils/icons";
 import {
   Sheet,
@@ -11,6 +11,10 @@ import {
 import IconCard from "./IconCard";
 
 const IconList = () => {
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    console.log(search);
+  }, [search]);
   return (
     <Sheet>
       <SheetTrigger className="hover:scale-110 transition-all duration-300">
@@ -20,14 +24,24 @@ const IconList = () => {
       </SheetTrigger>
       <SheetContent className="overflow-auto">
         <SheetHeader>
+          <SheetTitle>Search Icons</SheetTitle>
+          <input
+            type="text"
+            placeholder=" Start searching..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="border-2 p-2 rounded-full"
+          />
           <SheetTitle>Select Icons</SheetTitle>
           <SheetDescription>
             <div className="flex flex-wrap">
-              {iconNames.map((iconName, index) => (
-                <div key={index} className="">
-                  <IconCard name={iconName} />
-                </div>
-              ))}
+              {iconNames
+                .filter((iconNames) => iconNames.toLowerCase().includes(search.toLowerCase().replace(/\s+/g, '')))
+                .map((iconName, index) => (
+                  <div key={index} >
+                    <IconCard name={iconName} />
+                  </div>
+                ))}
             </div>
           </SheetDescription>
         </SheetHeader>
